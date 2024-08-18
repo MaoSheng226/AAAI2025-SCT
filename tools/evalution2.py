151159ec013b8fa76ad08a4d3eea47394a937785
MaoSheng226 <2813981587@qq.com>
2024-08-18 17:49:06 +0800

    Update evalution2.py

--- tools/evalution2.py
+++ tools/evalution2.py
@@ -9,8 +9,8 @@ SM = M.Smeasure()
 EM = M.Emeasure()
 MAE = M.MAE()
 
-mask_root = '/userHome/zy/Hms/ACCoNet/dataset/train_dataset/EORSSD/test-labels/'
-pred_root = '/userHome/zy/Hms/ACCoNet/EORSSD'
+mask_root = ''  # GT path
+pred_root = ''  # result map path
 mask_name_list = sorted(os.listdir(mask_root))
 for mask_name in tqdm(mask_name_list, total=len(mask_name_list)):
     mask_path = os.path.join(mask_root, mask_name)
@@ -31,13 +31,12 @@ mae = MAE.get_results()['mae']
 
 print(
     'Smeasure:', sm.round(4), '; ',
-    'wFmeasure:', wfm.round(4), '; ',
-    'MAE:', mae.round(4), '; ',
+    'maxFm:', fm['curve'].max().round(4),
+    'meanFm:', fm['curve'].mean().round(4), '; ',
+    'adpFm:', fm['adp'].round(4), '; ',
+    'maxEm:', '-' if em['curve'] is None else em['curve'].max().round(4), '; ',
     'adpEm:', em['adp'].round(4), '; ',
     'meanEm:', '-' if em['curve'] is None else em['curve'].mean().round(4), '; ',
-    'maxEm:', '-' if em['curve'] is None else em['curve'].max().round(4), '; ',
-    'adpFm:', fm['adp'].round(4), '; ',
-    'meanFm:', fm['curve'].mean().round(4), '; ',
-    'maxFm:', fm['curve'].max().round(4),
+    'MAE:', mae.round(4), '; ',
     sep=''
 )
